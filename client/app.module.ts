34133f7e1bb0810ecdbcb50d4e156e0bbf36d225
Guillaume <guillaume.latour@gmail.com>
2016-08-25 00:31:52 -0400

    ngrx counter???
    lots of fixes in node_modules :/ import not working by default in
    multiple ngrx packages…

--- client/app.module.ts
+++ client/app.module.ts
@@ -1,5 +1,8 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { NgModule, enableProdMode } from '@angular/core';
+import { Store, StoreModule, ActionReducer, Action } from '@ngrx/store/src';
+import { instrumentStore } from '@ngrx/store-devtools';
+import { useLogMonitor } from '@ngrx/store-log-monitor';
 import { BrowserModule }  from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { FormsModule }   from '@angular/forms';
@@ -21,15 +24,40 @@ import { MediumEditorComponent } from './app/shared/medium-editor/medium-editor.
 if (process.env.ENV === 'production') 
   enableProdMode();
 
+//---
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+export const RESET = 'RESET';
+
+export const counterReducer: ActionReducer<number> = (state: number = 0, action: Action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case DECREMENT:
+            return state - 1;
+        case RESET:
+            return 0;
+        default:
+            return state;
+    }
+}
+
 @NgModule({
   imports: [
     BrowserModule,
     HttpModule,
     FormsModule,
+    StoreModule.provideStore({counter: counterReducer}, {counter: 0}),
     RouterModule.forRoot(routes, { useHash: true })
   ],
   declarations: [  MediumEditorComponent, AppComponent, Home, Header, ContentView, ContentCreateComponent, ContentEditComponent, ContentFormComponent ],
   bootstrap: [ AppComponent ],
-  providers: [ MustsService ]
+  providers: [ MustsService,
+    instrumentStore({
+        monitor: useLogMonitor({
+            visible: true,
+            position: 'right'
+        })
+    }) ]
 })
 export class AppModule { }
--- client/app/home/home.component.ts
+++ client/app/home/home.component.ts
@@ -1,23 +1,40 @@
 import { Component, Output } from '@angular/core';
-
+import { Store } from '@ngrx/store/src';
+import { StoreLogMonitorComponent } from '@ngrx/store-log-monitor';
+import { Observable } from 'rxjs';
 import { ContentTileComponent } from '../content/content-tile.component';
 import { MustsService } from '../shared/services/musters.service';
+
+interface AppState {
+  counter: number;
+}
 
 @Component( {
   selector: 'home',
   templateUrl: 'home.component.html',
   providers: [ MustsService ],
   styles: [ "h5 {display: inline; }" ],
-  directives: [ ContentTileComponent ]
+  directives: [ StoreLogMonitorComponent, ContentTileComponent ]
 })
 export class Home {
   error = "";
   musts = [];
+  counter: Observable<{}>;
 
-  constructor( private service: MustsService ) { }
+  constructor( private service: MustsService, public store: Store<AppState> ) {
+     this.counter = store.select('counter');
+   }
 
   ngOnInit() {
     this.getMusts();
+
+    setTimeout(() => {
+
+    this.store.dispatch({type: 'INCREMENT'});
+    this.store.dispatch({type: 'INCREMENT'});
+    this.store.dispatch({type: 'INCREMENT'});
+
+    }, 1000);
   }
 
   onDelete() {
